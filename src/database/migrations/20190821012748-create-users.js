7bc67c8b15c871c6471cf894232edd121744f47f
Gabriela Mendes <contato@gabrielamendes.dev>
2019-08-22 22:04:36 -0300

    User controller

--- src/database/migrations/20190821012748-create-users.js
+++ src/database/migrations/20190821012748-create-users.js
@@ -8,7 +8,7 @@ module.exports = {
           autoIncrement: true,
           primaryKey: true
         },
-        nome: {
+        name: {
           type: Sequelize.STRING,
           allowNull: false
         },
